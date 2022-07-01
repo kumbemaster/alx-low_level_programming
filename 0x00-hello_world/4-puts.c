0420f51cecef64d471b116163ae4e4e906752aca
kumbemaster <kumbemaster@gmail.com>
2022-06-30 20:35:05 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* main is the entry point
+ * code is in block enclosed in curly bracket
+ * return is 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
