780ad8b1a6b58cebc2f1f31cec2500dcfa8d000a
kumbemaster <kumbemaster@gmail.com>
2022-06-30 21:09:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 
 {
@@ -7,5 +14,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
