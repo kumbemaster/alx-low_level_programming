aa047b867f2cce53351e5bdedc3f3a6cf00e1dba
kumbemaster <kumbemaster@gmail.com>
2022-07-02 16:39:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase, q, e;
+	q = 'q';
+	e = 'e';
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if (lowercase != q && lowercase != e)
+			putchar(lowercase);
+	
+	}
+
+	putchar('\n');
+
+	return (0);
+}
