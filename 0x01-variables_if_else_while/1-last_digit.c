201126cf49264d8492f0f4dac756f2c3cd96c735
kumbemaster <kumbemaster@gmail.com>
2022-07-02 15:24:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,6 @@ int main(void)
 	else if (Y == 0)
 		printf("%s %d is %d and is 0\n", str, n, Y);
 	else if (Y < 6)
-		printf("%s %d is %d and is less than 6 and not 0\n", str, n, Y);	
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, Y);
 	return (0);
 }
