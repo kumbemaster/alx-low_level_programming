b2bff60eebe65269b33e826035725c25d722ab84
kumbemaster <kumbemaster@gmail.com>
2022-07-05 15:28:50 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - prints putchar
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char p = "_putchar\n";
+	int i;
+	char p = "_putchar";
+
+	for (i = 0; i <= 10; i++)
+	while(p)
+	{	
+		_putchar(p);
+		p++;
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02. C - Functions, nested loops
+
+This is about the C programming function and nested loops in ALX Software Engineering
