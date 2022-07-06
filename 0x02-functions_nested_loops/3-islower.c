08ac51a9f8a3f3731d3bbf54f72fc6d2cd8fd094
kumbemaster <kumbemaster@gmail.com>
2022-07-05 17:36:04 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowecase characters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	
+	else
+		return (0);
+	
+
+}
