cdd3653f068f8a8c2db2690624e3b3a1d88a6fad
kumbemaster <kumbemaster@gmail.com>
2022-07-05 17:50:31 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for alphabetic character whether lowercase or uppercase
+ *@c: single letter input
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c);
+{
+
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+
+	else
+		return (0);
+
+}
