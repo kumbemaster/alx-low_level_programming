c661758b5adcf0397020aed6d36de6d269411cba
kumbemaster <kumbemaster@gmail.com>
2022-07-06 05:11:22 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,20 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		return (1);	
 		_putchar('o');
+		return (1);	
 	}
 	else if (n == 0)
 	{	
+		_putchar('0');
 		return (0);
-		_putchar('r');
 	}
 	
 	else
 	{
+		_putchar('-');
 		return (-1);
-		_putchar('q');
+			
 	}
 	_putchar('\n');
 }
