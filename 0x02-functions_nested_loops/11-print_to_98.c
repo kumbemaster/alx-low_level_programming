221b811cd1c670dea69fdf17eff354e30bdb8c78
kumbemaster <kumbemaster@gmail.com>
2022-07-06 16:08:43 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 			printf("%d", n);
 			if (n != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 			n++;
 
@@ -34,7 +34,7 @@ void print_to_98(int n)
 
 			if (n != 98)
 			{
-				printf("%d", n);
+				printf(", ");
 			}
 			n--;
 		}
