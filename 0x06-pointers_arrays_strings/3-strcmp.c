4f50106739c7861ab8e1b157dc41809ace073892
kumbemaster <kumbemaster@gmail.com>
2022-07-14 16:15:45 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+i#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: first input
+ * @s2: second input
+ * Return:  Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+
+}
