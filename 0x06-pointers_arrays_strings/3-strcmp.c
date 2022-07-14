81ddea9d71231b0a5818d10135a64ba1de5b88cc
kumbemaster <kumbemaster@gmail.com>
2022-07-14 16:32:07 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,17 @@ i#include "main.h"
 int _strcmp(char *s1, char *s2)
 {
 
-	int result;
+	int i;
 
-	result = strcmp(s1, s2);
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 
-	return (result);
+	{
+
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+
+	return (0);
 
 }
