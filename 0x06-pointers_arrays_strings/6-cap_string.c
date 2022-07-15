81c549dbeeab65204ba10781d127f34a4d5d59be
kumbemaster <kumbemaster@gmail.com>
2022-07-15 03:18:35 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes all words of a string
+ * @n: input string
+ * Return:  string
+ */
+
+char *cap_string(char *n)
+{
+
+	int x = 0, i;
+	int l = 13;
+	char spec[] = {' ', '\n', '\t', ',', ';', '.', '?', '"', '(', ')', '{', '}'};
+
+	while (n[x])
+
+	{
+		i = 0;
+		while (i < l)
+		{
+		if ((x = 0 || n[x - 1] == spec[i] && n[x] >= 97 && n[x] <= 122)
+			n[x] = n[x] - 32;
+		x++;
+	}
+
+		return (n);
+
+}
