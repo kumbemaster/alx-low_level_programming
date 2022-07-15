d54951eeb5ae46669b43b4ca7b0cb88cd5c73f42
kumbemaster <kumbemaster@gmail.com>
2022-07-15 03:44:21 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *n)
 
 	int x = 0, i;
 	int l = 13;
-	char spec[] = {' ', '\n', '\t', ',', ';', '.', '?', '"', '(', ')', '{', '}'};
+	char spec[] = {' ', '\t', '\n', ',', ';', '.', '?', '"', '(', ')', '{', '}'};
 
 	while (n[x])
 
@@ -19,7 +19,7 @@ char *cap_string(char *n)
 		i = 0;
 		while (i < l)
 		{
-		if ((x = 0 || n[x - 1] == spec[i]) && n[x] >= 97 && n[x] <= 122)
+		if ((x == 0 || n[x - 1] == spec[i]) && n[x] >= 97 && s[x] <= 122)
 			n[x] = n[x] - 32;
 			i++;
 		}
