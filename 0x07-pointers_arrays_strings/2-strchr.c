e8c719065d47a810e75098aa265eb909945812c9
kumbemaster <kumbemaster@gmail.com>
2022-07-18 17:05:02 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _strchr - function that locates a character in string
+ * @c: character
+ * @s: string
+ * Return: first occurence of c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + 1);
+	}
+
+	return (NULL);
+
+}
